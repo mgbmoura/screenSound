235fd6bedc5de5a233b792ee2a849e5570f3b9ab
Marcelo de Moura <113573345+mgbmoura@users.noreply.github.com>
2024-01-26 15:38:27 -0300

    projetoScreenSound.

--- P-1_screenSound/obj/Release/net8.0/P-1_screenSound.AssemblyInfo.cs
+++ P-1_screenSound/obj/Release/net8.0/P-1_screenSound.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("P-1_screenSound")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("P-1_screenSound")]
--- screenSound/obj/Release/net8.0/screenSound.AssemblyInfo.cs
+++ screenSound/obj/Release/net8.0/screenSound.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("P-1_screenSound")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("screenSound")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("P-1_screenSound")]
-[assembly: System.Reflection.AssemblyTitleAttribute("P-1_screenSound")]
+[assembly: System.Reflection.AssemblyProductAttribute("screenSound")]
+[assembly: System.Reflection.AssemblyTitleAttribute("screenSound")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
